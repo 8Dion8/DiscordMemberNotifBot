bd06ced6c6fcf77ec1ac6e1f9d800c298d8770c4
8Dion8 <shvartserinfo@gmail.com>
2020-12-12 11:05:07 +0300

    upd

--- beta.py
+++ beta.py
@@ -1,9 +1,43 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
-@client.event
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print('Бот онлайн епт')
+    channel = bot.get_channel(775400125144498186)
+    await channel.send('Бот онлайн епт')
+
+
+@bot.command()
+async def online(ctx):
+    online_members = []
+    offline_members = []
+    channel = bot.get_channel(775400125144498186)
+    channel.send(ctx.guild.members)
+    async for user in ctx.guild.fetch_members(limit=None):
+        if user.status == discord.Status.offline:
+            offline_members.append(user.name + '#' + user.discriminator)
+        else:
+            online_members.append(user.name + '#' + user.discriminator)
+
+    print(online_members,offline_members)
+    await channel.send(f'Я вижу {len(online_members)} додика без социальной жизни и {len(offline_members)} человека которые спят')
+
+
+@bot.command()
+async def stfu(ctx):
+    channel = bot.get_channel(775400125144498186)
+    await channel.send('А нахуй не пойдешь? Будешь выебываться - @Dion тебя забанит')
+@bot.command()
+async def cum(ctx):
+    channel = bot.get_channel(775400125144498186)
+    await channel.send(file=discord.File('maxresdefault.jpg'))
+
+bot.run(TOKEN)
